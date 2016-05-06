62def91c0e6f1383ad1d7439ac1805f844070e3d
Luis Felipe <lusemli@icloud.com>
2016-05-05 20:07:01 -0400

    Initial Commit

---
+++ lxmUnidad2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lxmUnidad2
+//
+//  Created by Luis Felipe on 5/5/16.
+//  Copyright © 2016 Luis Felipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
